c84435167fc7f9451642cd2a003ccdd282bd10cf
Ryan Evan Wolf <ryanevwolf@gmail.com>
2017-12-10 14:23:38 +0200

    removed old structure

--- Structures/cmake/OpenCV---backup.cmake
+++
@@ -1,4 +0,0 @@
-set(OpenCV_DIR /home/ryan/OpenCV2_Builds/install)
-set(OpenCV_INCLUDE_DIRS /home/ryan/OpenCV2_Builds/install/include)
-set(OpenCV_LIBRARY_DIRS /home/ryan/OpenCV2_Builds/install/lib)
-set(OpenCV_LIBS opencv_core opencv_highgui opencv_imgproc)
